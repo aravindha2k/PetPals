19b1f346bedf00fd051207ac5aa90697aa495047
raushanrauss <151566432+raushanrauss@users.noreply.github.com>
2024-05-09 04:49:31 +0530

    connect to atlas

--- Backend/Routes/userRouter.js
+++ Backend/Routes/userRouter.js
@@ -1,10 +1,15 @@
 const express=require('express');
 const { userModel } = require('../AllSchema/userSchema');
 const userRouter=express.Router()
+var jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 userRouter.post("/signup", async (req, res) => {
+    const{email,username,password}=req.body;
+    const saltRound=10;
     try {
-      const user = new userModel(req.body);
+        const hashedPass=await bcrypt.hash(password,saltRound);
+      const user = new userModel({email,password:hashedPass,username});
       user.save();
       res.status(200).send(user);
     } catch (err) {
@@ -13,16 +18,26 @@ userRouter.post("/signup", async (req, res) => {
     }
   });
   userRouter.post("/login", async (req, res) => {
-    const{email,password}=req.body;
+    const { email, password } = req.body;
     try {
-      const user =  userModel.findOne({email});
-      user.save();
-      res.status(200).send(user);
+        // Find user by email
+        const user = await userModel.findOne({ email });
+
+        // If user not found or password doesn't match, return error
+        if (!user || user.password !== password) {
+            return res.status(401).send("Invalid email or password");
+        }
+
+        // Generate token
+        const token = jwt.sign({ userId: user._id }, 'your_secret_key', { expiresIn: '1h' });
+
+        // Send token in response
+        res.status(200).json({ token });
     } catch (err) {
-      console.log(err);
-      res.status(401).send("Error");
+        console.error(err);
+        res.status(500).send("Internal Server Error");
     }
-  });
+});
 
 
 
