6332fff4a749d6460dff94253d17d90d9143665a
raushanrauss <151566432+raushanrauss@users.noreply.github.com>
2024-05-08 22:21:45 +0530

    connected to atlas

--- Backend/index.js
+++ Backend/index.js
@@ -1,23 +1,13 @@
 const express = require("express");
 const connectDb = require("./config/connectDb");
-const { userModel } = require("./AllSchema/userSchema");
+const userRouter = require("./Routes/userRouter");
 const app = express();
 require("dotenv").config();
 app.use(express.json());
-app.get("/", (req, res) => {
-  res.send("Getting");
-});
+app.use("/user",userRouter)
 
-app.post("/signup", async (req, res) => {
-  try {
-    const user = new userModel(req.body);
-    user.save();
-    res.status(200).send(user);
-  } catch (err) {
-    console.log(err);
-    res.status(401).send("Error");
-  }
-});
+
+
 app.listen(process.env.PORT, async () => {
   console.log(`Server is running at ${process.env.PORT}`);
   await connectDb();
